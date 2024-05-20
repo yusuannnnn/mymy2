1b02ef5eeb73afce2eca40de7207b5d692c60a64
yusuannnnn <yusuan1013@gmail.com>
2024-05-20 16:28:20 +0800

    add

--- Web/Services/LineService.cs
+++ Web/Services/LineService.cs
@@ -36,6 +36,11 @@ public class LineService : ILineService
         return  new List<ISendMessage>
         {
                 new ImageMessage(imgeUrl[Index]"https://megapx-assets.dcard.tw/images/3b3f8969-8fa8-421e-ad6a-349e0fd4f4b1/1280.jpeg",null),
+                
+                
+                new TextMessage(($"Receive a text event message \nchannelId={channelId}  \nuserId={userId}")
+        };
+        return result;
         };
         
         
